5872900f113cfa6b2740d3fecc4e1362db248c2d
JuliusB-ui <baraquiel.julius@gmail.com>
2024-05-05 23:53:42 +0800

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+// Additional styles import
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
